d0bc31662b35ded501aa15034dfb56456c59e7b4
Alexey Michurin <a.michurin@gmail.com>
2014-06-29 19:10:59 +0400

    drop permissions asap

--- radio-paradise-extension/js/rp_link.js
+++ radio-paradise-extension/js/rp_link.js
@@ -14,10 +14,17 @@
 'use strict';
 
 (function () {
+
+  var permissions = {
+    permissions: ['tabs']
+  };
+
+  function remove_permissions() {
+    chrome.permissions.remove(permissions);
+  }
+
   window.document.getElementById('rp-link').onclick = function () {
-    chrome.permissions.request({
-      permissions: ['tabs'],
-    }, function (granted) {
+    chrome.permissions.request(permissions, function (granted) {
       if (granted) {
         chrome.tabs.query({
           url: '*://*.radioparadise.com/*'
@@ -25,14 +32,15 @@
           if (tabs.length) {
             chrome.tabs.update(tabs[0].id, {
               active: true
-            });
+            }, remove_permissions);
           } else {
             chrome.tabs.create({
               url: 'http://www.radioparadise.com/'
-            });
+            }, remove_permissions);
           }
         });
       }
     });
   };
+
 }());
