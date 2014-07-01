5b7d72fea3a1b9911ebebdfd065225e8126af3f2
Alexey Michurin <a.michurin@gmail.com>
2014-07-01 06:09:03 +0400

    minor cosmetics

---
+++ radio-paradise-extension/js/reload.js
@@ -0,0 +1,22 @@
+/*
+ * Radio Paradise player
+ * Copyright (c) 2014 Alexey Michurin <a.michurin@gmail.com>
+ * MIT License [http://www.opensource.org/licenses/mit-license.php]
+ */
+
+/*global window, chrome */
+/*jslint
+  indent:   2,
+  vars:     true,
+  plusplus: true
+*/
+
+'use strict';
+
+(function () {
+
+  window.document.getElementById('reload').onclick = function () {
+    chrome.runtime.reload();
+  };
+
+}());
