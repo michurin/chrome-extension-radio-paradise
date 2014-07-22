7a029a44584a113322f12efb3bfe45c982594199
Alexey Michurin <a.michurin@gmail.com>
2014-07-23 01:00:11 +0400

    switch to corresponding chrome window if link have to open in existing tab in other window

--- radio-paradise-extension/js/open_tab.js
+++ radio-paradise-extension/js/open_tab.js
@@ -29,13 +29,35 @@ var open_url_in_new_tab = (function () {
         chrome.tabs.query({
           url: '*://*.radioparadise.com/*'
         }, function (tabs) {
+          var tab, tab_selector;
           if (tabs.length) {
-            chrome.tabs.update(tabs[0].id, {
-              active: true,
-              url: target_url
-            }, remove_permissions);
+            tab = tabs[0];
+            tab_selector = function () {
+              var tab_options = {
+                active: true
+              };
+              if (tab.url !== target_url) {
+                tab_options.url = target_url;
+              }
+              chrome.tabs.update(tab.id, tab_options, remove_permissions);
+            };
+            chrome.windows.getCurrent(undefined, function (w) {
+              var window_options;
+              if (w.id !== tab.windowId) {
+                window_options = {
+                  focused: true
+                };
+                if (w.state === 'minimized') {
+                  window_options.state = 'normal';
+                }
+                chrome.windows.update(tab.windowId, window_options, tab_selector);
+              } else {
+                tab_selector();
+              }
+            });
           } else {
             chrome.tabs.create({
+              active: true,
               url: target_url
             }, remove_permissions);
           }
