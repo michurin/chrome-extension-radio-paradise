46c991523ef2b887dea3ce3771c3423365646d21
Alexey Michurin <a.michurin@gmail.com>
2014-07-26 12:17:40 +0400

    split animation facilities

--- radio-paradise-extension/js/animator.js
+++ radio-paradise-extension/js/animator.js
@@ -5,7 +5,7 @@
  */
 
 /*global window */
-/*exported height_animator_generator */
+/*exported animator_generator */
 
 'use strict';
 
@@ -22,73 +22,12 @@ function animator_generator(cb_step, cb_fin) {
     }
   }
   return function (strategy) {
+    // strategy will be changed!
+    // it can be useful side effect
     var l = current_strategy.length;
     current_strategy = strategy;
     if (l === 0) {
       step();
     }
-  };
-}
-
-function height_animator_generator(wrapper, container) {
-  var animator = animator_generator(function (v) {
-    container.style.height = v.height;
-    container.style.opacity = v.opacity;
-  }, function () {
-    container.style.overflow = 'visible';
-    container.style.height = 'auto';
-    container.style.opacity = 1;
-  });
-  function set_content(content) {
-    var i;
-    container.innerText = '';
-    for (i = 0; i < content.length; ++i) {
-      container.appendChild(content[i]);
-    }
-  }
-  return function (content, no_animation) {
-    // content may be
-    // (i) array with elements to use or
-    // (ii) initial height if content already exists in root element
-    if (no_animation) {
-      set_content(content);
-      return;
-    }
-    var predefined_content = (typeof content === 'number');
-    var ih;
-    if (predefined_content) {
-      ih = content;
-    } else {
-      ih = container.clientHeight;
-    }
-    container.style.overflow = 'visible';
-    container.style.height = 'auto';
-    wrapper.style.overflow = 'hidden';
-    wrapper.style.height = wrapper.clientHeight + 'px';
-    if (!predefined_content) {
-      set_content(content);
-    }
-    var fh = container.clientHeight;
-    container.style.overflow = 'hidden';
-    container.style.height = ih + 'px';
-    container.style.opacity = 0;
-    wrapper.style.overflowY = 'visible';
-    wrapper.style.height = 'auto';
-    animator((function (a, b) {
-      var f, x, y, m, i, s = [], J = 20;
-      m = b - a;
-      for (i = 0; i < J; ++i) {
-        // like f = Math.sin(i/J * Math.PI/2) but better
-        x = i / J; // 0..1
-        y = 4 - x * 3; // 4..1
-        y *= y; // 16..1
-        f = (16 - y) / 15; // 0..1
-        s.push({
-          height: Math.round(a + m * f) + 'px',
-          opacity: x * x
-        });
-      }
-      return s;
-    }(ih, fh))); // start animation
   };
 }
