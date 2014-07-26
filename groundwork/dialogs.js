632d678d30363a20e6ec31d89d0dfb78f2719cba
Alexey Michurin <a.michurin@gmail.com>
2014-07-26 23:53:16 +0400

    groundwork: animate opacity

--- groundwork/dialogs.js
+++ groundwork/dialogs.js
@@ -7,19 +7,39 @@
 // dialoger
 
 var dialogs_generator = (function () {
+  var seq_forward = [];
+  var seq_backward = [];
+  (function (i) {
+    for (i = 0; i < 1; i += 0.1) {
+      seq_forward.push(i);
+      seq_backward.push(1 - i);
+    }
+  }());
   var dialogs = document.getElementById('dialogs');
+  var dialogs_sentinel = document.getElementById('dialogs-sentinel');
+  function animation_step(v) {
+    dialogs.style.opacity = v;
+  }
   return function (name) {
     var e = document.getElementById(name);
-    var a = height_animator_generator(e, e.querySelector('div'));
+    var a = animator_generator(animation_step, function () { // fin
+      dialogs.style.opacity = 1;
+      dialogs_sentinel.style.display = 'none';
+    });
+    var b = animator_generator(animation_step, function () { // fin
+      dialogs.style.display = 'none';
+      e.style.display = 'none';
+    });
     return {
       open: function () {
+        a(seq_forward.slice(0));
         dialogs.style.display = 'block';
+        dialogs_sentinel.style.display = 'block';
         e.style.display = 'block';
-        a(0);
       },
       close: function () {
-        dialogs.style.display = 'none';
-        e.style.display = 'none';
+        b(seq_backward.slice(0));
+        dialogs_sentinel.style.display = 'block';
       }
     }
   }
